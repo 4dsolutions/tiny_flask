ba51d8777b36b72684d4a5e8db842ae74ae3dc8f
Kirby Urner <kirby.urner@gmail.com>
2016-08-17 14:05:30 -0700

    sponsored by...

--- static/js/home.js
+++ static/js/home.js
@@ -1,6 +1,7 @@
 /**
  * 
  */
-function triangle(x){
-	return x * x;
+function sponsored_by () {
+	var sponsor = "<a href='http://www.4dsolutions.net'><i>4D Solutions</i></a>"
+	document.getElementById("sponsor").innerHTML = "Brought to you by " + sponsor;
 }
