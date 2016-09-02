ac500df5434d3af94725d2e445de4e464288f9e9
Kirby Urner <kirby.urner@gmail.com>
2016-09-01 17:11:56 -0700

    updating

---
+++ static/js/home.js
@@ -0,0 +1,7 @@
+/**
+ * 
+ */
+function sponsored_by () {
+	var sponsor = "<a href='http://www.4dsolutions.net'><i>4D Solutions</i></a>"
+	document.getElementById("sponsor").innerHTML = "Brought to you by " + sponsor;
+}
