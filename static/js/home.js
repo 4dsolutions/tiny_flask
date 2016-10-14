8f7e40ecc837056d8c36402708219e083e6f4cbe
Kirby Urner <kirby.urner@gmail.com>
2016-10-14 15:30:18 -0700

    new improved

--- static/js/home.js
+++ static/js/home.js
@@ -5,3 +5,19 @@ function sponsored_by () {
 	var sponsor = "<a href='http://www.4dsolutions.net'><i>4D Solutions</i></a>"
 	document.getElementById("sponsor").innerHTML = "Brought to you by " + sponsor;
 }
+
+function goHome() {
+	window.location = "home";
+}
+
+function goTerms() {
+	window.location = "glossary";
+}
+
+function goElements() {
+	window.location = "elements";
+}
+
+function goShapes() {
+	window.location = "shapes";
+}
